21817d1b32f5355892807e3b767a77158b143f6a
73UtkarshMudgal <mudgalua@rknec.edu>
2024-10-29 16:13:11 +0530

    Added form for user input

--- index.php
+++ index.php
@@ -9,6 +9,28 @@
 
 <body>
     <h1>Welcome to the Greet & Eat!</h1>
+    <form action="" method="POST">
+        <label for="name" class="highlight name-label">Name:</label>
+        <input type="text" id="name" name="name" required aria-label="Enter your name">
+
+        <label class="highlight food-label">Favorite Food:</label>
+        <div class="radio-group">
+            <label>
+                <input type="radio" id="pulao" name="food" value="Pulao" required>
+                Pulao
+            </label>
+            <label>
+                <input type="radio" id="paneer" name="food" value="Paneer" required>
+                Paneer
+            </label>
+            <label>
+                <input type="radio" id="pizza" name="food" value="Pizza" required>
+                Pizza
+            </label>
+        </div>
+
+        <button type="submit">Submit</button>
+    </form>
 </body>
 
 </html>
